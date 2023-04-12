f001c7f693cbdb3a6e9165eb896b50694e81e2a4
Anika Thordarson <thors@rackham4.uppmax.uu.se>
2023-04-12 21:10:45 +0200

    Finished the quality checking, output files all in place.

--- 01_analyses/00_preprocessing/00_quality_check/README.txt
+++ 01_analyses/00_preprocessing/00_quality_check/README.txt
@@ -1,1 +1,3 @@
-All output files from running FastQC on all the raw data files. If tmp exists, I'm doing testing there and it will be deleted when I do the final analysis.
+All output files from running FastQC on all the raw data files. 
+If tmp exists, I'm doing testing there and it will be deleted when I do the final analysis.
+There are zip files and html files for each of the analysis, the files are named like the raw data is. 
--- 02_code/00_preprocessing/00_quality_check.sh
+++ 02_code/00_preprocessing/00_quality_check.sh
@@ -3,8 +3,8 @@
 #SBATCH -A uppmax2023-2-8
 #SBATCH -M snowy
 #SBATCH -p core
-#SBATCH -n 1
-#SBATCH -t 5:00
+#SBATCH -n 2
+#SBATCH -t 45:00
 #SBATCH -J GA_qual
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user anth2886@student.uu.se
@@ -14,9 +14,6 @@ module load bioinfo-tools FastQC/0.11.9/
 
 # Commands
 export SRCDIR=$HOME/2023_GA/GA_Project/
-cd $SNIC_TMP
 
-fastqc -t 1 $SRCDIR/00_data/00_raw_data/01_RNA_untrimmed/RNA_D1_1*
-
-cp RNA* $SRCDIR/01_analyses/00_preprocessing/00_quality_check/tmp/
-
+fastqc -t 2 $SRCDIR/00_data/00_raw_data/00_DNA_trimmed/DNA* -o $SRCDIR/01_analyses/00_preprocessing/00_quality_check/tmp
+fastqc -t 2 $SRCDIR/00_data/00_raw_data/01_RNA_untrimmed/RNA* -o $SRCDIR/01_analyses/00_preprocessing/00_quality_check/tmp
