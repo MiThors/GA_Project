974acf513d66f44acf20842ce65700e58f167a04
Anika Thordarson <thors@rackham4.uppmax.uu.se>
2023-04-13 17:50:35 +0200

    Revert "Set up so I can run the trimming on the second half of the data."
    This reverts commit a3f3a732e699355d8a146863ee74c030f1d394b0.
    
    Conflicts:
    	02_code/00_preprocessing/02_RNA_trimming.sh

--- 02_code/00_preprocessing/02_RNA_trimming.sh
+++ 02_code/00_preprocessing/02_RNA_trimming.sh
@@ -20,6 +20,7 @@ OUT="01_analyses/00_preprocessing/02_trimming/tmp"
 
 cd $SNIC_TMP
 
+<<<<<<< HEAD
 java -jar /sw/bioinfo/trimmomatic/0.39/rackham/trimmomatic.jar PE -threads 2 -phred33 -trimlog $SRCDIR/$OUT/log.txt $SRCDIR/$DATA/RNA_D3_1.fastq.gz $SRCDIR/$DATA/RNA_D3_2.fastq.gz $SRCDIR/$OUT/RNA_D3_PF.fq.gz $SRCDIR/$OUT/RNA_D3_UF.fq.gz $SRCDIR/$OUT/RNA_D3_PR.fq.gz $SRCDIR/$OUT/RNA_D3_UR.fq.gz ILLUMINACLIP:/sw/bioinfo/trimmomatic/0.39/rackham/adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36 
 =======
 export SRCDIR=$HOME/2023_GA/GA_Project/
@@ -28,3 +29,6 @@ cd $SNIC_TMP
 
 java -jar /sw/bioinfo/trimmomatic/0.39/rackham/trimmomatic PE -threads 2 -phred33 
 >>>>>>> 74a47a853441e9784d78d7610bab65944fb05c75
+=======
+java -jar /sw/bioinfo/trimmomatic/0.39/rackham/trimmomatic.jar PE -threads 2 -phred33 -trimlog $SRCDIR/$OUT/log.txt $SRCDIR/$DATA/RNA_D1_1.fastq.gz $SRCDIR/$DATA/RNA_D1_2.fastq.gz $SRCDIR/$OUT/RNA_D1_PF.fq.gz $SRCDIR/$OUT/RNA_D1_UF.fq.gz $SRCDIR/$OUT/RNA_D1_PR.fq.gz $SRCDIR/$OUT/RNA_D1_UR.fq.gz ILLUMINACLIP:/sw/bioinfo/trimmomatic/0.39/rackham/adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36 
+>>>>>>> parent of a3f3a73... Set up so I can run the trimming on the second half of the data.
