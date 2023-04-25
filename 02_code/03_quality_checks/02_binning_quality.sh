5733afe8f849f68a9602dbe357dd70d94918d825
Anika Thordarson <thors@rackham4.uppmax.uu.se>
2023-04-25 16:09:29 +0200

    The report from the assembly has been added, and I ran the binning quality check.

--- 02_code/03_quality_checks/02_binning_quality.sh
+++ 02_code/03_quality_checks/02_binning_quality.sh
@@ -3,22 +3,34 @@
 #SBATCH -A uppmax2023-2-8
 #SBATCH -M snowy
 #SBATCH -p core
-#SBATCH -n 2
-#SBATCH -t 1:30:00
-#SBATCH -J GA_binng
+#SBATCH -n 4
+#SBATCH -t 2:00:00
+#SBATCH -J GA_qual_bin
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user anth2886@student.uu.se
 
 # Load modules
-module load bioinfo-tools MetaBat/2.12.1
-echo "Loaded MetaBat"
+module load bioinfo-tools CheckM/1.1.3
 
 # Commands
 export SRCDIR=$HOME/2023_GA/GA_Project/
-DATA="00_data/03_assembled_DNA/DNA_assembled.fa.gz"
+DATA="00_data/04_binning/"
 
 cd $SNIC_TMP
+mkdir CheckM_data
+cd CheckM_data
+cp -av $CHECKM_DATA/checkm_data_2015_01_16.tar.gz .
+mkdir 2015_01_16
+cd 2015_01_16
+tar xzf ../checkm_data_2015_01_16.tar.gz
+checkm data setRoot $PWD
 
-metabat2 -i $SRCDIR/$DATA -o $SRCDIR/01_analyses/02_binning/tmp/
+cd $SNIC_TMP 
+mkdir tmp
+cp $SRCDIR/$DATA/bin* tmp 
+gunzip tmp/bin*
+
+checkm lineage_wf -t 4 --reduced_tree -x fa tmp $SRCDIR/01_analyses/03_quality_checks/01_binning_quality/tmp 
+
 
 
