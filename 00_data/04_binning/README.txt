604d55a583c434c674cf6d28d5d7869cff527e55
Anika Thordarson <thors@rackham4.uppmax.uu.se>
2023-04-25 15:20:25 +0200

    Added a data folder for the soft links to the bins, and ran the assembly quality again.

---
+++ 00_data/04_binning/README.txt
@@ -0,0 +1,2 @@
+All 51 bin files as soft links, removing the "." that are in the filenames from MetaBat so they can be run on CheckM.
+
--- 02_code/03_quality_checks/01_assembly_quality.sh
+++ 02_code/03_quality_checks/01_assembly_quality.sh
@@ -5,7 +5,7 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 1:45:00
-#SBATCH -J GA_binng
+#SBATCH -J GA_qual_ass
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user anth2886@student.uu.se
 
@@ -15,9 +15,10 @@ module load bioinfo-tools quast/5.0.2
 # Commands
 export SRCDIR=$HOME/2023_GA/GA_Project/
 DATA="00_data/03_assembled_DNA/DNA_assembled.fa.gz"
+QUAST_HOME=/sw/bioinfo/quast/5.0.2/snowy/bin
 
 cd $SNIC_TMP
 
-python metaquast.py $SRCDIR/$DATA -o $SRCDIR/01_analyses/03_quality_checks/00_assembly_quality/tmp/ -t 2
+python $QUAST_HOME/metaquast.py $SRCDIR/$DATA -o $SRCDIR/01_analyses/03_quality_checks/00_assembly_quality/tmp/ -t 2
 
 
