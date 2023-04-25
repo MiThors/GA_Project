81ce1b686575fb042b4e4c2614f11759afa52841
Anika Thordarson <thors@rackham1.uppmax.uu.se>
2023-04-25 14:17:23 +0200

    Should be ready to run quality check on assembly now.

--- 02_code/03_quality_checks/01_assembly_quality.sh
+++ 02_code/03_quality_checks/01_assembly_quality.sh
@@ -4,14 +4,13 @@
 #SBATCH -M snowy
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 1:30:00
+#SBATCH -t 1:45:00
 #SBATCH -J GA_binng
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user anth2886@student.uu.se
 
 # Load modules
-module load bioinfo-tools MetaBat/2.12.1
-echo "Loaded MetaBat"
+module load bioinfo-tools quast/5.0.2
 
 # Commands
 export SRCDIR=$HOME/2023_GA/GA_Project/
@@ -19,6 +18,6 @@ DATA="00_data/03_assembled_DNA/DNA_assembled.fa.gz"
 
 cd $SNIC_TMP
 
-metabat2 -i $SRCDIR/$DATA -o $SRCDIR/01_analyses/02_binning/tmp/
+python metaquast.py $SRCDIR/$DATA -o $SRCDIR/01_analyses/03_quality_checks/00_assembly_quality/tmp/ -t 2
 
 
