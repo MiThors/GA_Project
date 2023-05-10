80b3dbf3ccb8884d1a4b6c4b802f5d52c2a0486a
Anika Thordarson <thors@rackham4.uppmax.uu.se>
2023-05-10 16:10:10 +0200

    Mapping set up, about to run.

--- 02_code/05_mapping/01_mapping.sh
+++ 02_code/05_mapping/01_mapping.sh
@@ -11,11 +11,42 @@
 
 # Load modules
 cd $SNIC_TMP
-module load 
+module load bioinfo-tools
+module load bwa/0.7.17
+module load samtools/1.14
 
 # Commands
 export SRCDIR=$HOME/2023_GA/GA_Project
-DATA="/00_data/04_binning"
+DNA="/00_data/04_binning"
+RNA="/00_data/02_trimmed_RNA"
 
 cd $SNIC_TMP
+cp $SRCDIR$DNA/bin* .
+# gunzip bin*
 
+n=0
+for i in bin*;
+do
+
+n=$((n+1))
+bwa index $i
+
+# D1
+bwa mem -t 2 $i \
+$SRCDIR$RNA/RNA_trimmed_D1_1.fastq.gz \
+$SRCDIR$RNA/RNA_trimmed_D1_2.fastq.gz \
+| samtools view -b - | samtools sort -o map_D1_bin$i.bam -
+samtools index map_D1_bin$i.bam
+samtools idxstats map_D1_bin$i.bam > map_D1_bin$i.stats
+ 
+# D2
+bwa mem -t 2 $i \
+$SRCDIR$RNA/RNA_trimmed_D3_1.fastq.gz \
+$SRCDIR$RNA/RNA_trimmed_D3_2.fastq.gz \
+| samtools view -b - | samtools sort -o map_D3_bin$i.bam -
+samtools index map_D3_bin$i.bam
+samtools idxstats map_D3_bin$i.bam > map_D3_bin$i.stats
+
+done
+
+cp map* $SRC/01_analyses/05_mapping/tmp
